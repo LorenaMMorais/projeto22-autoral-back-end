a803eb131c65ff369940fffe21a082b08fe4d1b3
Lorena Morais <lorenamorais@alunos.utfpr.edu.br>
2023-06-04 18:50:17 -0300

    Revert "feat: add users"
    This reverts commit 29121e6006cdfd9a95ec00562ea615f4ffe46771.

--- src/middlewares/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './error-handlign-middleware';
-export * from './validation-middleware';
-export * from './authentication-middleware';
--- src/schemas/users-schemas.ts
+++
@@ -1,8 +0,0 @@
-import Joi from 'joi';
-import { CreateUserParams } from '@/services/users-service';
-
-export const createUserSchema = Joi.object<CreateUserParams>({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(8).required(),
-    role: Joi.boolean().required()
-});
